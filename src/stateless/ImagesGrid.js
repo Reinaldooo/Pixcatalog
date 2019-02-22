31cb3e84e316c61605f5d418ad3bd2136acc2122
Reinaldo Trindade <rewifetri@gmail.com>
2019-02-22 14:33:51 -0300

    README Update

--- src/stateless/ImagesGrid.js
+++ src/stateless/ImagesGrid.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
-import { blue } from '../utils/colors';
+// import { blue } from '../utils/colors';
 
 function importAllImages(r) {
   let images = {};
